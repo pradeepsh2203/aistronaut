6baa69a67417befd3f580110bed49443ee7037be
Harsh Kumar Mishra <harshkumarmishra@192.168.1.2>
2025-02-08 16:01:38 +0530

    added streamlit frontend

--- index.py
+++ index.py
@@ -5,9 +5,9 @@ import os
 
 
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-
-img_path = "img/img_10.png"
-img = cv2.imread(img_path)
-
-objs = DeepFace.analyze(img)
-print(objs)
+def emotion_check(img_array):
+    # img_path = img_path
+    # img = cv2.imread(img_path)
+    objs = DeepFace.analyze(img_array)
+    return objs
+   
