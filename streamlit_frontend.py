f9233a817edfabe1b114466ea1eec3372a9256ff
Pradeep Sharma <pradeepsharma.sharma095@gmail.com>
2025-02-09 20:00:54 +0530

    Final with best UI

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -11,7 +11,7 @@ from worker_agents.neutral_agent import neutral_agent
 from utilities.videoHtml import returnVideoHtml
 import os
 def main():
-    st.title("Image Processing with Streamlit")
+    st.title("Astronaut Buddy")
 
     # File uploader (more robust way)
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -28,42 +28,42 @@ def main():
         # with open(temp_filename, "wb") as f:
         #     f.write(bytes_data)
         
-        st.write("Uploaded Image:")
+        st.write("Astronaut's Image:")
         st.image(uploaded_file, caption="Uploaded Image.", use_container_width=True)
 
         # Call the image processing function
         # result = emotion_check(temp_filename)
-        result =emotion_check(img_array)
+        with st.spinner("Processing..."):
+            result =emotion_check(img_array)
 
 
         if result:
-            st.success(result)
-            # You can display other results from process_image() here
-            # agent= CustomLLM()
-            # print(result[0]["dominant_emotion"],result[0]["age"],result[0]["dominant_race"],result[0]["dominant_gender"])
-            # resp = agent.run(emotion=result[0]["dominant_emotion"],age=result[0]["age"], race=result[0]["dominant_race"], gender=result[0]["dominant_gender"])
-            # st.write(resp)
+            st.write("Emotion detected:", result[0]["dominant_emotion"])
+            st.write("Age detected:", result[0]["age"])
+            st.write("Gender detected:", result[0]["dominant_gender"])
+            st.write("Race detected:", result[0]["dominant_race"])
+
+
             if(result[0]["dominant_emotion"]=="sad"):
                 sadPrompt = f"Create a image of a animal to lighten the mood of a person who is feeling sad. Use img html to display the image. also create a joke to make the person laugh. Use features of the user to make the joke more relatable, use h1 tag for the joke. age:{result[0]['age']} race:{result[0]['dominant_race']} gender:{result[0]['dominant_gender']}. your response should be in html format."
-                response = sad_agent.run(sadPrompt)
+                with st.spinner("Processing..."):
+                    response = sad_agent.run(sadPrompt)
+                st.success("Below is what I can do to make you feel better")
                 st.html(response)
             elif(result[0]["dominant_emotion"]=="happy"):
                 happyPrompt = f"Find a good dance or song video for the user to vibe on by using the YouTube tool, ensuring that the search query contains no commas and personalizing the content using the user’s age ({result[0]['age']}), race ({result[0]['race']}), and gender ({result[0]['dominant_gender']}); respond in the exact format 'Thought: extract youtube urls', 'Action: [specify the tool you want to use]', and 'Action Input: [provide the input to that tool]'"
-                response = happy_agent.run(happyPrompt)
-                print(response)
+                with st.spinner("Processing..."):
+                    response = happy_agent.run(happyPrompt)
+                # print(response)
+                st.success("Below is what I can do to make you feel better")
                 result = returnVideoHtml(response)
-                # print(result)
-                # st.html(result)
-            # elif(result[0]["dominant_emotion"]=="angry"):
-            #     response = angry_agent.run("Provide a detailed research on Mahakumbh Mela")
-            #     st.write(response)
             else:
                 neutralPrompt = f"Write an poem in the user's native language that is deeply motivational and uplifting. Tailor the content to resonate with the user by incorporating their demographic details: age ({result[0]['age']}), race ('{result[0]['dominant_race']}'), and gender ({result[0]['dominant_gender']}). The poem should emphasize positivity in life, human values, and the importance of national pride and identity, celebrating the beauty and heritage of their culture while inspiring the reader to embrace their potential and contribute positively to society.Also add few motivating quotes in the poem. Once you have the poem completed use the text_to_speech tool to convert the entire poem to audio format."
-                response = neutral_agent.run(neutralPrompt)
-                # st.write(response)
+                with st.spinner("Processing..."):
+                    response = neutral_agent.run(neutralPrompt)
                 dist = os.path.join("./tools/audio.wav")
+                st.success("Below is what I can do to make you feel better")
                 st.audio(dist, format='audio/wav')
-                # os.remove(dist)
 
     
 
